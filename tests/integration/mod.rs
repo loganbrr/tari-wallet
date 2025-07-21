f696b64556bd0a041f84b3ed6308fbbb9d526995
Krakaw <41575888+Krakaw@users.noreply.github.com>
2025-07-21 16:29:43 -0700

    Add proto download

---
+++ tests/integration/mod.rs
@@ -0,0 +1,9 @@
+//! Integration test module
+//!
+//! This module contains integration tests that test the interaction between
+//! multiple components of the Tari lightweight wallet system.
+
+pub mod wallet_workflow;
+pub mod scanning_workflow;
+pub mod transaction_workflow;
+pub mod multi_network_compatibility;
