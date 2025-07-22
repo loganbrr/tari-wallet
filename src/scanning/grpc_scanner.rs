736da789956f3668b96eea4fa522eed57d0fbe17
Krakaw <41575888+Krakaw@users.noreply.github.com>
2025-07-22 08:23:25 -0700

    Fix doctest

--- src/scanning/grpc_scanner.rs
+++ src/scanning/grpc_scanner.rs
@@ -10,7 +10,7 @@
 //!
 //! ```rust,no_run
 //! #[cfg(feature = "grpc")]
-//! use lightweight_wallet_libs::scanning::{GrpcBlockchainScanner, ScanConfig, BlockchainScanner};
+//! use lightweight_wallet_libs::scanning::grpc_scanner::{GrpcBlockchainScanner, ScanConfig, BlockchainScanner};
 //! #[cfg(feature = "grpc")]
 //! use lightweight_wallet_libs::wallet::Wallet;
 //!
